f6c015ce8595ca53d395db3e6aad01513d6bc020
joaopbueno <mrjoao16@gmail.com>
2024-10-12 10:36:13 -0300

    StackView de Previsão por Hora

---
+++ WeatherApp/Color+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extensions.swift
+//  WeatherApp
+//
+//  Created by João Bueno on 12/10/24.
+//
+
+import Foundation
---
+++ WeatherApp/HourlyForecastCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HourlyForecastCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by João Bueno on 12/10/24.
+//
+
+import UIKit
+
+class HourlyForecastCollectionViewCell: UICollectionViewCell {
+    
+}
